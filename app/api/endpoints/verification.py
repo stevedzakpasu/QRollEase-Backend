2a731878a66f90e657fc55a2d96f9b80bac36699
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2023-06-04 19:21:57 +0000

    added checks for verification code expiry

--- app/api/endpoints/verification.py
+++ app/api/endpoints/verification.py
@@ -4,27 +4,35 @@ from app.api.deps import get_current_active_user
 from app.core.deps import get_session
 from app.core.security import generate_and_send_verification_code, check_is_staff
 from app.models.user import User
-
+import datetime
 
 
 
 router = APIRouter()
 @router.post("/users/verify_code", dependencies=[Depends(get_current_active_user)])
 def verify_code(*, session: Session = Depends(get_session),
-    code: str, current_user = Depends(get_current_active_user)):
-    if current_user.verification_code == code:
+                code: str,
+                current_user=Depends(get_current_active_user)):
+    current_time = datetime.datetime.now()
+    expiry_date = current_user.verification_code_expiry
+
+    if current_user.verification_code == code and current_time < expiry_date:
         current_user.is_verified = True
         if check_is_staff(current_user.email):
             current_user.is_staff = True
-
         session.commit()
-    
-    else:   
+
+    elif current_user.verification_code != code:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Invalid verification code"
         )
-
+    
+    else:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Verification code has expired"
+        )
 
     return {"message": "Verification complete"}
 
