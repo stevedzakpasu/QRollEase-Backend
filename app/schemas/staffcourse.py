eb1b65fdd7f712099a2678fcd262711ea0ea68ad
Stephen Dzakpasu <steve.dzakpasu@gmail.com>
2023-08-24 18:23:32 +0000

    update staffcourse model to allow course deletion

--- app/schemas/staffcourse.py
+++ app/schemas/staffcourse.py
@@ -1,8 +1,10 @@
-from sqlmodel import SQLModel, Field
+from sqlmodel import SQLModel, Field, String, Column, ForeignKey
 
 
 
 class StaffCourseBase(SQLModel):
     staff_id: str = Field(foreign_key="staff.staff_id", primary_key=True)
-    course_code: str = Field(foreign_key="course.course_code", primary_key=True)
+    course_code: str =  Field(
+        sa_column=Column(String, ForeignKey("course.course_code", ondelete="CASCADE"))  # Set the foreign key behavior on the table metadata
+    )
 
